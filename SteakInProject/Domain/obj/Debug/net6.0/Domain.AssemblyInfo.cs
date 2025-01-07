a408e9be9ba9777d4ac463305d85a59d9fc362fc
Nergiz Nadirli <nergiznadirli@Nergiz-MacBook-Air.local>
2025-01-07 21:46:06 +0400

    contact page but some problem

--- SteakInProject/Domain/obj/Debug/net6.0/Domain.AssemblyInfo.cs
+++ SteakInProject/Domain/obj/Debug/net6.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc50baf087860aea644533818d3d03dd23d20df7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
