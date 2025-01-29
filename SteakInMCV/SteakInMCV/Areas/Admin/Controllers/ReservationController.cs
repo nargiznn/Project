c0fdd1ef1e4f734074ec8b73f5353805050ba5f1
Nergiz Nadirli <nergiznadirli@Nergiz-MacBook-Air.local>
2025-01-29 18:34:49 +0400

    reservation admin

---
+++ SteakInMCV/SteakInMCV/Areas/Admin/Controllers/ReservationController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SteakInMCV.Areas.Admin.ViewModels.Award;
+using SteakInMCV.Areas.Admin.ViewModels.Reservation;
+using SteakInMCV.Models;
+using SteakInMCV.Models.Enum;
+using SteakInMCV.ViewModels;
+
+namespace SteakInMCV.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ReservationController:Controller
+	{
+        private readonly string BaseURl = "http://localhost:7031"; 
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<ReservationViewModel> reservationVMs = null;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{BaseURl}/api/reservations/GetReservations"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationVMs = JsonConvert.DeserializeObject<IEnumerable<ReservationViewModel>>(apiResponse);
+                }
+            }
+            return View(reservationVMs);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateStatus(int id)
+        {
+            ReservationViewModel reservation = null;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{BaseURl}/api/Reservations/GetReservationById/{id}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservation = JsonConvert.DeserializeObject<ReservationViewModel>(apiResponse);
+                }
+            }
+            return View(reservation);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateStatus(int id, ReservationStatus status)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(status), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync($"{BaseURl}/api/Reservations/UpdateReservationStatus/update-status/{id}", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return BadRequest(apiResponse);
+                    }
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int id)
+        {
+            ReservationViewModel award = null;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{BaseURl}/api/Reservations/GetReservationById/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    award = JsonConvert.DeserializeObject<ReservationViewModel>(apiResponse);
+                }
+            }
+
+            return View(award);
+        }
+    }
+}
+
---
+++ SteakInMCV/SteakInMCV/Areas/Admin/ViewModels/Reservation/ReservationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SteakInMCV.Models.Enum;
+
+namespace SteakInMCV.Areas.Admin.ViewModels.Reservation
+{
+	public class ReservationViewModel
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public int PeopleCount { get; set; }
+        public ReservationStatus Status { get; set; }
+    }
+}
+
